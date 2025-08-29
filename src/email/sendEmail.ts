26db00f764902b0f774d5d89c1a47683ba2d677c
Soham Haldar <sohamhaldar25@gmail.com>
2025-08-30 05:02:34 +0530

    Used nodemailer and removed external services

--- src/email/sendEmail.ts
+++ src/email/sendEmail.ts
@@ -1,8 +1,9 @@
 import { Resend } from "resend";
 import { render } from '@react-email/components';
 import email_template from "@/email/email_template";
-import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
-import postmark from 'postmark';
+// import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
+// import postmark from 'postmark';
+import nodemailer from 'nodemailer';
 
 interface ApiResponse{
   success:Boolean;
@@ -15,15 +16,15 @@ export async function sendVerificationEmail(
   otp: string
 ): Promise<ApiResponse> {
   const resend=new Resend(process.env.RESEND_API_KEY);
-  const mailerSend = new MailerSend({
-    apiKey: process.env.MAILERSEND_API_KEY || '',
-  });
+  // const mailerSend = new MailerSend({
+  //   apiKey: process.env.MAILERSEND_API_KEY || '',
+  // });
 
-  const client = new postmark.ServerClient(process.env.POSTMARK_API_KEY||'');
+  // const client = new postmark.ServerClient(process.env.POSTMARK_API_KEY||'');
 
 
-  const recipients = [new Recipient(email, username)];
-  const sentFrom = new Sender(process.env.MAILERSEND_EMAIL_DOMAIN||'', "BoardSync");
+  // const recipients = [new Recipient(email, username)];
+  // const sentFrom = new Sender(process.env.MAILERSEND_EMAIL_DOMAIN||'', "BoardSync");
   
   const emailTemplate=render(email_template({ username, otp }),{
     pretty:true
@@ -37,20 +38,34 @@ export async function sendVerificationEmail(
 
     // mailerSend.email.send(emailParams);
 
-    // const options = {
-    //   From: 'you@example.com',
-    //   To: email,
-    //   Subject: "BoardSync Onboarding Verification Code",
-    //   HtmlBody: emailTemplate,
-    // };
-    
-    // await client.sendEmail(options);
-    await resend.emails.send({
-      from: 'BoardSync <onboarding@resend.dev>',
+    const options = {
+      from: 'BoardSync <boardsyncmail@gmail.com>',
       to: email,
       subject: "BoardSync Onboarding Verification Code",
       html: emailTemplate,
+    };
+    
+    // await client.sendEmail(options);
+
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "boardsyncmail@gmail.com",
+        pass: process.env.GOOGLE_APP_PASSWORD,
+      },
     });
+    await transporter.sendMail(options);
+
+    
+    // await resend.emails.send({
+    //   from: 'BoardSync <onboarding@resend.dev>',
+    //   to: email,
+    //   subject: "BoardSync Onboarding Verification Code",
+    //   html: emailTemplate,
+    // });
 
 
     return { success: true, message: 'Verification email sent successfully.' };
